00f4bbdb174f8277462b93e952682bd94539f545
jvleao01 <jvcleao0509@gmail.com>
2026-08-13 17:14:11 -0300

    Add files via upload

---
+++ Fin_Control/backend/models/lancamento.py
@@ -0,0 +1,15 @@
+class Lancamento:
+
+    def __init__(
+        self,
+        tipo,
+        categoria,
+        descricao,
+        valor,
+        data
+    ):
+        self.tipo = tipo
+        self.categoria = categoria
+        self.descricao = descricao
+        self.valor = valor
+        self.data = data
