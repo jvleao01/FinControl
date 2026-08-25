bd94932063b83666a0e676052cec98a0b9540efd
João Victor Cezario Leão <jvcleao0509@gmail.com>
2026-08-25 20:47:31 -0300

    Add files via upload

---
+++ Fin_Control/backend/database/init_db.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+DATABASE = os.path.join(
+    BASE_DIR,
+    "banco.db"
+)
+
+conexao = sqlite3.connect(DATABASE)
+
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS lancamentos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tipo TEXT NOT NULL,
+    categoria TEXT,
+    descricao TEXT NOT NULL,
+    valor REAL NOT NULL,
+    data TEXT NOT NULL
+)
+""")
+
+conexao.commit()
+conexao.close()
+
+print("Banco criado com sucesso")
